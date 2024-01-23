5c2da0d62b38f55c4a7733ee3210ac256ddc1bb1
Anna Foix <afoix@ebi.ac.uk>
2024-01-23 21:55:57 +0000

    use wandblogger to log info (mean, std dev...)

--- scripts/_shape_embed.py
+++ scripts/_shape_embed.py
@@ -494,13 +494,18 @@ def shape_embed_process(clargs):
     trial_df.to_csv(metadata("trial_df.csv"))
     trial_df.groupby("trial").mean().to_csv(metadata("trial_df_mean.csv"))
     trial_df.plot(kind="bar")
-
-    # Special metrics for f1 score for wandb
-    #wandb.log({"trial_df": wandb.Table(dataframe=trial_df)})
+    
     #mean_df = trial_df.groupby("trial").mean()
     #std_df = trial_df.groupby("trial").std()
-    #wandb.log({"Mean": wandb.Table(dataframe=mean_df)})
-    #wandb.log({"Std": wandb.Table(dataframe=std_df)})
+    #wandb.log_table(mean_df)
+    #wandb.log_table(std_df) 
+    
+    #Special metrics for f1 score for wandb
+    wandblogger.experiment.log({"trial_df": wandb.Table(dataframe=trial_df)})
+    mean_df = trial_df.groupby("trial").mean()
+    std_df = trial_df.groupby("trial").std()
+    wandblogger.experiment.log({"Mean": wandb.Table(dataframe=mean_df)})
+    wandblogger.experiment.log({"Std": wandb.Table(dataframe=std_df)})
 
     melted_df = trial_df.melt(id_vars="trial", var_name="Metric", value_name="Score")
     # fig, ax = plt.subplots(figsize=(width, height))
