292de473d5c7974a1d846b78ed1d021d2606aba7
Craig Russell <ctr26@ebi.ac.uk>
2024-10-01 12:00:09 +0100

    [bug] forgot to update pyproject toml

--- scripts/idr.py
+++
@@ -1,5 +0,0 @@
-# %% [markdown]
-# root = "/nfs/ftp/public/databases/IDR/"
-
-# %% [markdown]
-# dataset = datasets.ImageFolder(transform=transform)
--- shape_embed/__init__.py
+++ shape_embed/__init__.py
@@ -1,26 +1,8 @@
 import torch
 
 torch.cuda.empty_cache()
-# from . import models, lightning, cli, export, config
-from .lightning import AESupervised, AEUnsupervised, AE,  AutoEncoderSupervised, AutoEncoderUnsupervised, AutoEncoder
 
-# TODO: Fix this import as it currently produces too many warnings
-from .models import ModelFactory, create_model
-from .bie import BioImageEmbed
-from .config import Config
-from . import augmentations
 
 import logging
 logging.captureWarnings(True)
 
-__all__ = [
-    "AESupervised",
-    "AutoEncoderUnsupervised",
-    "AEUnsupervised",
-    "AutoEncoderSupervised",
-    "AutoEncoder"
-    "AE"
-    "BioImageEmbed",
-    "Config",
-    "augmentations",
-]
